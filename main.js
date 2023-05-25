ea787006bcb8b921879c3d89c065cdcd02c0868c
Austin Dean <19595957+AustinDean39@users.noreply.github.com>
2023-05-25 17:42:53 -0400

    feat: Allow user to change resolution of canvas

--- main.js
+++ main.js
@@ -1,9 +1,30 @@
 // INITIALIZE THE CANVAS WITH A 16X16 GRID
 createCanvas(16);
 
+// CONNECT THE SLIDER INPUT TO THE CANVAS SIZE
+const sizeSetting = document.getElementById('canvas-size-input');
+const resetCanvasBtn = document.getElementById('reset-button');
+
+updateSizeText();
+
+resetCanvasBtn.addEventListener('click', function () {
+  createCanvas(sizeSetting.value);
+});
+
+// DISPLAY CHOSEN SIZE BENEATH THE SLIDER, UPDATE AS NEEDED
+sizeSetting.addEventListener('input', updateSizeText);
+
+// FUNCTION TO UPDATE TEXT UNDERNEATH SIZE SETTING SLIDER
+function updateSizeText() {
+  const sliderText = document.getElementById('size-settings-text');
+  const newText = `${sizeSetting.value} x ${sizeSetting.value}`;
+  sliderText.textContent = newText;
+}
+
 // FUNCTION TO FILL THE CANVAS WITH CORRECT NUMBER OF SQUARES
 function createCanvas(squares) {
   const canvas = document.getElementById('canvas');
+  canvas.innerHTML = '';
   for (let i = 0; i < squares; i++) {
     const row = document.createElement('div');
     row.classList.add('row');
