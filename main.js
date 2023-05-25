f798bd39cc399ad67438682b2d5cae0f51a3dc52
Austin Dean <19595957+AustinDean39@users.noreply.github.com>
2023-05-25 17:42:53 -0400

    feat: Add color change to squares when clicked

--- main.js
+++ main.js
@@ -14,7 +14,17 @@ function createCanvas(squares) {
 
   console.log(canvas.childNodes);
 
+  const columns = Array.from(document.getElementsByClassName('column'));
+  columns.forEach(square => {
+  square.addEventListener('click', changeColor);
+});
+
   return;
 }
 
 createCanvas(16);
+
+// FUNCTION TO CHANGE BACKGROUND COLOR OF A DIV
+function changeColor(element) {
+  element.target.style.background = 'black';
+}
